c5336afc31cc6569d122d0508813681c85870d97
syuanivy <syuan6@usfca.edu>
2014-09-29 15:22:22 -0700

    SynchronizedBlockingQueue 2370.229ms 3375201.3events/second

--- src/cs601/blkqueue/SynchronizedBlockingQueue.java
+++ src/cs601/blkqueue/SynchronizedBlockingQueue.java
@@ -1,15 +1,48 @@
 package cs601.blkqueue;
 
+import java.util.LinkedList;
+
 public class SynchronizedBlockingQueue<T> implements MessageQueue<T> {
+	int size;
+	LinkedList<T> queue;
+	
 	public SynchronizedBlockingQueue(int size) {
+		this.size = size;
+		queue = new LinkedList<T> ();
 	}
 
 	@Override
 	public void put(T o) throws InterruptedException {
+		synchronized (this) {
+			while(true){
+				if(queue.size()<size){
+					queue.add(o);
+					break;
+				}else{
+					wait();
+
+				}
+			}
+			notifyAll();
+		}
 	}
 
 	@Override
 	public T take() throws InterruptedException {
-		return null;
+		T retrieve;
+		synchronized(this){
+			while(true){
+				if(queue.size()>0){
+				retrieve = queue.remove();
+				break;
+				}else{
+					wait();
+				}
+			}
+			notifyAll();
+		}
+		return retrieve;
+
+			
 	}
 }
